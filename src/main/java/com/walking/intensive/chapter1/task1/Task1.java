89986887d826e50d660c3d5ef3c5fe58d835ecac
Александр <Sasha_D_32@mail.ru>
2024-04-26 15:41:51 +0700

    repeat2 Chapter Task1

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -2,23 +2,24 @@ package com.walking.intensive.chapter1.task1;
 
 public class Task1 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 58;
 
-        System.out.println("Вам " + age + " " + getAgeString(age%100));
+        int age = 5;
+
+        System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
 
-
-
-        if (age % 10 == 1 && age != 11)
-            return "год";
-
-        else if (age > 4 && age < 21 || age % 10 > 4 || age % 10 == 0)
-            return "лет";
-
-        else
-            return "года";
+        if (age < 0 || age > 127) {
+            return "Некорректно введен возраст";
+        }
+        int age2 = age % 100;
+        if (age2 % 10 == 1 && age2 != 11) {
+            return "Вам " + age + " год";
+        }
+        if (age2 > 4 && age2 < 21 || age2 % 10 > 4 || age2 % 10 == 0) {
+            return "Вам " + age + " лет";
+        }
+        return "Вам " + age + " года";
     }
 }
