a09b9933d32e0dc28e87de4b36fe1e73fe6f0c22
Александр <Sasha_D_32@mail.ru>
2024-04-26 12:36:37 +0700

    repeat Chapter Task1

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -1,19 +1,24 @@
 package com.walking.intensive.chapter1.task1;
 
-/**
- * Условие: <a href="https://geometry-math.ru/homework/Java-age.html">ссылка</a>
- */
 public class Task1 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
+        int age = 58;
 
-        System.out.println(getAgeString(age));
+        System.out.println("Вам " + age + " " + getAgeString(age%100));
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+
+
+        if (age % 10 == 1 && age != 11)
+            return "год";
+
+        else if (age > 4 && age < 21 || age % 10 > 4 || age % 10 == 0)
+            return "лет";
+
+        else
+            return "года";
     }
 }
