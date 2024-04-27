d2dd89108e22f9b2bb40a75fcaff1c0d94499c08
Александр <Sasha_D_32@mail.ru>
2024-04-27 11:31:07 +0700

    repeat3 Chapter Task1

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -2,24 +2,27 @@ package com.walking.intensive.chapter1.task1;
 
 public class Task1 {
     public static void main(String[] args) {
-
         int age = 5;
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-
         if (age < 0 || age > 127) {
             return "Некорректно введен возраст";
         }
-        int age2 = age % 100;
-        if (age2 % 10 == 1 && age2 != 11) {
+
+        int ageTens = age % 100;
+        int ageUnits = age % 10;
+
+        if (ageUnits == 1 && ageTens != 11) {
             return "Вам " + age + " год";
         }
-        if (age2 > 4 && age2 < 21 || age2 % 10 > 4 || age2 % 10 == 0) {
+
+        if (ageTens > 4 && ageTens < 21 || ageUnits > 4 || ageUnits == 0) {
             return "Вам " + age + " лет";
         }
+
         return "Вам " + age + " года";
     }
 }
--- src/main/java/com/walking/intensive/chapter1/task2/Task2.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Task2.java
@@ -6,7 +6,14 @@ package com.walking.intensive.chapter1.task2;
 public class Task2 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-
+    System.out.println("Hi");
+    int a1 = 2;
+    int b1 = 5;
+    int result = (b1 * 3 + 20 / 2 * a1--) + a1;
+    System.out.println(result);
+    int a=0;
+    int b = a++ + a++ + a++;
+    System.out.println(b +" "+ a);
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
